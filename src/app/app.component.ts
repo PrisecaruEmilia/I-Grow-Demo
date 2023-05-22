01642e6aea77ce33693acfa0790e58909a65471b
PrisecaruEmilia <prisecaru.emilia@gmail.com>
2023-05-23 00:27:46 +0300

    using ViewChild with elements - like button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { ChildComponent } from './child/child.component';
 
 @Component({
@@ -6,10 +6,19 @@ import { ChildComponent } from './child/child.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   @ViewChild(ChildComponent, { static: true }) child?: ChildComponent;
+  @ViewChild('button', { static: true })
+  buttonRef?: ElementRef<HTMLButtonElement>;
+
   increment() {
     console.log(this.child);
     this.child?.increment();
+  }
+
+  ngAfterViewInit(): void {
+    if (this.buttonRef?.nativeElement) {
+      this.buttonRef.nativeElement.innerHTML = 'after init';
+    }
   }
 }
