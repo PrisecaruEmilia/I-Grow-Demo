a6dbf39ea76d76e5ceac1ad50f841abe86a6364f
PrisecaruEmilia <prisecaru.emilia@gmail.com>
2023-05-23 00:22:27 +0300

    using ViewChild with components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-i-grow';
+  @ViewChild(ChildComponent, { static: true }) child?: ChildComponent;
+  increment() {
+    console.log(this.child);
+    this.child?.increment();
+  }
 }
