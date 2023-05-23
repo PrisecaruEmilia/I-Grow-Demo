bbbbe9be8d04c21ae3cef70675cd1fc4a5821b62
PrisecaruEmilia <prisecaru.emilia@gmail.com>
2023-05-24 00:41:59 +0300

    ngOnDestroy on action

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss'],
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, OnDestroy {
+  counter = 0;
+  interval: any;
+
   constructor() {
     console.log('Child constructor is called');
   }
+
   ngOnInit(): void {
     console.log('Child OnInit is called');
+    this.interval = setInterval(() => {
+      this.counter = this.counter + 1;
+      console.log(this.counter);
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    console.log('Child OnDestroy is called');
+    clearInterval(this.interval);
   }
 }
