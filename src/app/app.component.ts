31179c7cb2414a41dac50304ba16be367f85eace
PrisecaruEmilia <prisecaru.emilia@gmail.com>
2023-05-24 22:58:27 +0300

    custom pipes in action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'I love Angular';
+  users = [
+    { name: 'Denis', age: 23 },
+    { name: 'Matt', age: 30 },
+    { name: 'Adam', age: 18 },
+  ];
 
-  log() {
-    console.log(this.title);
+  onClickAdd(): void {
+    // this will work with an inpur pipe - BAD for perfomance
+    // the pipe it will run every single time because of change detection
+    // this.users.push({ name: 'Ben', age: 35 });
+
+    // use pure pipes and this
+    this.users = [...this.users, { name: 'Ben', age: 35 }];
   }
 }
---
+++ src/app/sort-list.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sortList',
+  pure: true,
+})
+export class SortListPipe implements PipeTransform {
+  transform(arr: any[], prop: string): any[] {
+    console.log('I run');
+    arr.sort((a: any, b: any) => {
+      if (a[prop] < b[prop]) {
+        return -1;
+      } else if (a[prop] > b[prop]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return arr;
+  }
+}
