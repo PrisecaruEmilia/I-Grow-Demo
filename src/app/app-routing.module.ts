f67f08cf4fbb3050d99896e87f9167090800ee8b
PrisecaruEmilia <prisecaru.emilia@gmail.com>
2023-05-24 23:25:30 +0300

    basic auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PrivateComponent } from './private/private.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   {
@@ -11,6 +12,7 @@ const routes: Routes = [
   {
     path: 'private',
     component: PrivateComponent,
+    canActivate: [AuthGuard],
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PrivateComponent } from './private/private.component';
+import { AuthGuard } from './auth.guard';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    PrivateComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HomeComponent, PrivateComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [AuthGuard],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
