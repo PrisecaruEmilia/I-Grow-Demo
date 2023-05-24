ec844202b8c7189b125ed8587aa65627fe40dcab
PrisecaruEmilia <prisecaru.emilia@gmail.com>
2023-05-24 22:25:00 +0300

    atribute directive in action

---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,14 @@
+import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[highlight]',
+})
+export class HighlightDirective implements AfterViewInit {
+  @Input() color: string = 'yellow';
+  constructor(private elementRef: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    console.log(this.elementRef);
+    this.elementRef.nativeElement.style.background = this.color;
+  }
+}
