3728cf180955683cfd0c956f04c2d16aec633124
PrisecaruEmilia <prisecaru.emilia@gmail.com>
2023-05-24 23:39:22 +0300

    implementing a real case scenario - auth guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CurrentUserService } from './current-user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-i-grow';
+export class AppComponent implements OnInit {
+  constructor(private currentUserService: CurrentUserService) {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.currentUserService.setCurrentUser();
+    }, 2000);
+  }
 }
--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -2,16 +2,43 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  Router,
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, filter, map } from 'rxjs';
+import { CurrentUserService } from './current-user.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  canActivate(): boolean {
-    return false;
+  constructor(
+    private currentUserService: CurrentUserService,
+    private router: Router
+  ) {}
+
+  canActivate(): Observable<boolean> {
+    // async
+
+    return this.currentUserService.currentUser$.pipe(
+      filter((currentUser) => currentUser !== undefined),
+      map((currentUser) => {
+        if (!currentUser) {
+          this.router.navigateByUrl('/');
+          return false;
+        }
+        return true;
+      })
+    );
   }
+
+  // sync
+
+  // canActivate(): boolean {
+  //   if (localStorage.getItem('token')) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
 }
