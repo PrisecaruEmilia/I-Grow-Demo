bac3f408ef99bc42c96b52a9957ba7dd7e2bffe6
PrisecaruEmilia <prisecaru.emilia@gmail.com>
2023-05-25 02:16:23 +0300

    ng-container in action

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.scss']
+  styleUrls: ['./child.component.scss'],
 })
 export class ChildComponent {
+  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+  // so if we want to show only the odd numbers, that is an additional condition
+  // so we need ng-container, and not another element, but why ??
 }
